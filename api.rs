fad7f2788ffb8155bd751206f3dd42613a498477
kowaioni <163346055+kowaioni@users.noreply.github.com>
2024-05-17 10:03:24 +0300

    change variable

--- api.rs
+++ api.rs
@@ -7,65 +7,65 @@ use actix_web::error::ResponseError;
 use std::fmt;
 
 #[derive(Debug)]
-enum AppError {
+enum WorkspaceError {
     NotFound(String),
-    LockFail(String),
+    MutexLockFailure(String),
 }
 
-impl fmt::Display for AppError {
+impl fmt::Display for WorkspaceError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            AppError::NotFound(ref message) => write!(f, "{}", message),
-            AppError::LockFail(ref message) => write!(f, "{}", message),
+            WorkspaceError::NotFound(ref message) => write!(f, "{}", message),
+            WorkspaceError::MutexLockFailure(ref message) => write!(f, "{}", message),
         }
     }
 }
 
-impl ResponseError for AppError {
+impl ResponseError for WorkspaceError {
     fn status_code(&self) -> StatusCode {
         match *self {
-            AppError::NotFound(_) => StatusCode::NOT_FOUND,
-            AppError::LockFail(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            WorkspaceError::NotFound(_) => StatusCode::NOT_FOUND,
+            WorkspaceError::MutexLockFailure(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
 
 #[derive(Serialize, Deserialize, Clone)]
-struct Project {
+struct ProjectDetail {
     id: usize,
     name: String,
-    tasks: Vec<Task>,
+    task_list: Vec<TaskDetail>,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
-struct Task {
+struct TaskDetail {
     id: usize,
-    name: String,
-    assigned_to: Option<String>,
-    status: String,
+    title: String,
+    assignee: Option<String>, 
+    current_status: String,
 }
 
-struct AppState {
-    projects: Mutex<Vec<Project>>,
+struct SharedState {
+    project_registry: Mutex<Vec<ProjectDetail>>,
 }
 
-async fn create_project(data: web::Json<Project>, app_state: web::Data<AppState>) -> Result<HttpResponse, AppError> {
-    let mut projects = app_state.projects.lock().map_err(|_| AppError::LockFail("Lock failed".into()))?;
-    projects.push(data.into_inner());
-    Ok(HttpResponse::Ok().json(projects.last()))
+async fn add_project(project_new: web::Json<ProjectDetail>, application_state: web::Data<SharedState>) -> Result<HttpResponse, WorkspaceError> {
+    let mut project_collection = application_state.project_registry.lock().map_err(|_| WorkspaceError::MutexLockFailure("Failed to acquire mutex lock".into()))?;
+    project_collection.push(project_new.into_inner());
+    Ok(HttpResponse::Ok().json(project_collection.last()))
 }
 
-async fn update_task_name(path: web::Path<(usize, usize)>, new_name: web::Json<String>, app_state: web::Data<AppState>) -> Result<HttpResponse, AppError> {
-    let mut projects = app_state.projects.lock().map_err(|_| AppError::LockFail("Lock failed".into()))?;
-    let (project_id, task_id) = path.into_inner();
+async fn modify_task_title(task_identifiers: web::Path<(usize, usize)>, new_title: web::Json<String>, application_state: web::Data<SharedState>) -> Result<HttpResponse, WorkspaceError> {
+    let mut project_collection = application_state.project_registry.lock().map_err(|_| WorkspaceError::MutexLockFailure("Failed to acquire mutex lock".into()))?;
+    let (project_identifier, task_identifier) = task_identifiers.into_inner();
     
-    if let Some(project) = projects.iter_mut().find(|p| p.id == project_id) {
-        if let Some(task) = project.tasks.iter_mut().find(|t| t.id == task_id) {
-            task.name = new_name.into_inner();
+    if let Some(project) = project_collection.iter_mut().find(|project| project.id == project_identifier) {
+        if let Some(task) = project.task_list.iter_mut().find(|task| task.id == task_identifier) {
+            task.title = new_title.into_inner();
             return Ok(HttpResponse::Ok().json(task));
         }
     }
-    Err(AppError::NotFound("Task or Project Not Found".into()))
+    Err(WorkspaceError::NotFound("Project or Task not found.".into()))
 }
 
 #[actix_rt::main]
@@ -73,19 +73,19 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    let server_address = env::var("SERVER_ADDRESS").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
+    let server_endpoint = env::var("SERVER_ADDRESS").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
 
-    let app_data = web::Data::new(AppState {
-        projects: Mutex::new(Vec::new()),
+    let shared_app_state = web::Data::new(SharedState {
+        project_registry: Mutex::new(Vec::new()),
     });
 
     HttpServer::new(move || {
         App::new()
-            .app_data(app_data.clone())
+            .app_data(shared_app_state.clone())
             .wrap(actix_web::middleware::Logger::default())
-            .route("/update_task_name/{project_id}/{task_id}", web::put().to(update_task_name))
+            .route("/project/task/modify/{project_id}/{task_id}", web::put().to(modify_task_title))
     })
-    .bind(&server_address)?
+    .bind(&server_endpoint)?
     .run()
     .await
 }
